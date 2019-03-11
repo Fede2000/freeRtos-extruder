7226a2c53972aa255dfcbe2c1cce4a026a492b3c
Fede2000 <fede.sarrocco@gmail.com>
2019-03-11 19:35:20 +0100

    temp version, ok: motor temp encoder

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@
 
 #define THERMISTOR_PIN 13
 #define HEATER_PIN 10
-double TempSetpoint = 100, steinhart;
+double TempSetpoint = 35, steinhart;
 
 
 int ESet = 1000;
@@ -49,13 +49,13 @@ U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
 
 // define two tasks for Blink & AnalogRead
-void TaskExtruder( void *pvParameters );
+//void TaskExtruder( void *pvParameters );
 void TaskTemperature( void *pvParameters );
 void TaskEncoder( void *pvParameters );
 void TaskDisplay( void *pvParameters );
 /* The service routine for the interrupt.  This is the interrupt that the task
 will be synchronized with. */
-static void vExampleInterruptHandler( void );
+//static void vExampleInterruptHandler( void );
 
 /* Declare a variable of type SemaphoreHandle_t.  This is used to reference the
 semaphore that is used to synchronize a task with an interrupt. */
@@ -71,11 +71,12 @@ AccelStepper extruder1(1, E_STEP_PIN, E_DIR_PIN);
 ClickEncoder encoder(ENCODER_PIN1, ENCODER_PIN2, ENCODER_BTN, 4);
 
 
-void timerIsr() { encoder.service(); }
+void timerIsr() { encoder.service(); } 
 
 void setup() {
   // serial init
   Serial.begin(9600);
+  pinMode(LED_BUILTIN,OUTPUT);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB  TODO: delete if serial port not used
   }
@@ -96,33 +97,54 @@ void setup() {
   extruder1.setSpeed(ESet*Ek);
   pinMode(HEATER_PIN, OUTPUT);
   digitalWrite(HEATER_PIN, LOW);
+
+  
   /* --------------------------------------Timer4 settings-------------------------------------*/
-  /* ------------ https://www.teachmemicro.com/arduino-timer-interrupt-tutorial/---------------*/
+  /* ------------ https://www.teachmemicro.com/arduino-timer-interrupt-tutorial/ ---------------*/
+  /* ---https://www.google.com/search?q=timer+arduino+mega&newwindow=1&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi1htP5v_rgAhWGx4UKHYSdDV8Q_AUIDygC&biw=1920&bih=969#imgrc=FSdu49NlAg2rrM: --*/
   // using a mask to protect other bits
-  TIMSK4 = (TIMSK4 & B11111001) | 010;    // Time interrupt mask register
-  TCCR4B = (TCCR4B & B11111000) | 010;   //prescaler: 8, freq = 16MHz --> 
-  OCR4A = 2000;    // compare register, callback every 1ms
+  //noInterrupts();
+  /*TIMSK2 = (TIMSK2 & B11111001) | 010;    // Time interrupt mask register
+  TCCR2B = (TCCR2B & B11111000) | 010;   //prescaler: 8, freq = 16MHz --> 
+  OCR2A = 100;    // compare register, callback every 1ms
+  */
+   TIMSK4 = (TIMSK4 & B11111101) | 0x06;
+   TCCR4B = (TCCR4B & B11111000) | 0x03;
+   OCR4A = 100;
+   OCR4B = 100;
+
+   
+  //interrupts();
   /* --------------------------------------------END------------------------------------------ */
    
    /* Before a semaphore is used it must be explicitly created.  In this example
       a binary semaphore is created. */
-  vSemaphoreCreateBinary( xBinarySemaphore );
-
+  //xSemaphoreCreateBinary( xBinarySemaphore );
+  //xBinarySemaphore = xSemaphoreCreateBinary();
+  //xBinarySemaphore = xSemaphoreCreateMutex();
+  //xSemaphoreGive( ( xBinarySemaphore ) );
+  /*if ( xBinarySemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  {
+    xBinarySemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Serial Port
+    if ( ( xBinarySemaphore ) != NULL )
+      xSemaphoreGive( ( xBinarySemaphore ) );  // Make the Serial Port available for use, by "Giving" the Semaphore.
+  }*/
+  //interrupts();
   // Now set up two tasks to run independently.
-  xTaskCreate(
+ /* xTaskCreate(
     TaskExtruder
     ,  (const portCHAR *)"Extruder"   // A name just for humans
     ,  128  // Stack size
     ,  NULL
     ,  4  // priority
     ,  NULL );
-
+*/
   xTaskCreate(
     TaskTemperature
     ,  (const portCHAR *) "Temperature"
     ,  128 // This stack size can be checked & adjusted by reading Highwater
     ,  NULL
-    ,  2  // priority
+    ,  3  // priority
     ,  NULL );
 
   xTaskCreate(
@@ -130,44 +152,46 @@ void setup() {
     ,  (const portCHAR *)"Encoder"   // A name just for humans
     ,  128  // Stack size
     ,  NULL
-    ,  3  // priority
+    ,  2  // priority
     ,  NULL );
 
-  xTaskCreate(
+  /*xTaskCreate(
     TaskDisplay
     ,  (const portCHAR *)"Display"   // A name just for humans
     ,  128  // Stack size
     ,  NULL
     ,  1  // priority
-    ,  NULL );
-
-
+    ,  NULL );*/
 }
 
 void loop() {
-  // leave it empty
+   // leave it empty
 }
 
 
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
-
+/*
 void TaskExtruder(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
 
   // initialize digital pin 13 as an output.
+  #define LONG_TIME 0xffff
 
   for (;;) // A Task shall never return or exit.
   {
 
-    xSemaphoreTake( xBinarySemaphore, ( TickType_t ) 0 );   // continuosly check for 
-    extruder1.runSpeed();
-    //vTaskDelay(1); // wait for one second
-    xSemaphoreGive( xBinarySemaphore );
+    //xSemaphoreTake( xBinarySemaphore, ( TickType_t ) portMAX_DELAY );   // continuosly check for portMAX_DELAY
+    if( xSemaphoreTake( xBinarySemaphore, 10 ) == pdTRUE ){
+      extruder1.runSpeed();
+    }
+    //extruder1.runSpeed();
+    //vTaskDelay(10); // wait for one second
+    //xSemaphoreGive( xBinarySemaphore );
   }
-}
+}*/
 
 void TaskTemperature(void *pvParameters)  // This is a task.
 {
@@ -244,8 +268,9 @@ void TaskEncoder(void *pvParameters)  // This is a task.
 
   // if change speed mode:
   if (lastButtonState == 5){
-    ESet += encoder.getValue();
+    ESet += encoder.getValue()*10;
     extruder1.setSpeed(ESet*Ek);
+    Serial.print("**NEW ESet: "); Serial.println(ESet);
   }
   // if change temperature mode:
   else if (lastButtonState == 6){
@@ -254,7 +279,6 @@ void TaskEncoder(void *pvParameters)  // This is a task.
   }
   // TODO: provare a inserirle nell switch
   //encPos += encoder.getValue();
-
 
   if (buttonState != 0) {
     Serial.print("Button: "); Serial.println(buttonState);
@@ -282,7 +306,7 @@ void TaskEncoder(void *pvParameters)  // This is a task.
         break;
     }
   }
-    vTaskDelay(5);
+    vTaskDelay(5); //5
   }
 }
 
@@ -298,21 +322,23 @@ void TaskDisplay(void *pvParameters)  // This is a task.
     //u8x8.drawString(0, 2, "T: ");
     u8x8.print(steinhart);  
     u8x8.refreshDisplay();
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    vTaskDelay(3); //1000 / portTICK_PERIOD_MS
+    
   }
 }
 
-
 // callback for timer4 
 ISR(TIMER4_COMPA_vect){
+  //static BaseType_t xHigherPriorityTaskWoken;
   TCNT4 = 0; // preload timer to 0 
-  //static signed portBASE_TYPE xHigherPriorityTaskWoken;
-
- // xHigherPriorityTaskWoken = pdFALSE;
-
+  extruder1.runSpeed();
+  //Serial.println("aa");
+  //xHigherPriorityTaskWoken = pdFALSE;
+  //xSemaphoreGiveFromISR( xBinarySemaphore, &xHigherPriorityTaskWoken );
   /* 'Give' the semaphore to unblock the task. */
-  xSemaphoreGiveFromISR( xBinarySemaphore, NULL); //(signed portBASE_TYPE*)&xHigherPriorityTaskWoken 
-
+  //xSemaphoreGiveFromISR( xBinarySemaphore, NULL); //(signed portBASE_TYPE*)&xHigherPriorityTaskWoken 
+  //xSemaphoreGive(xBinarySemaphore);
+  //digitalWrite(LED_BUILTIN,HIGH);
   /* xHigherPriorityTaskWoken was initialised to pdFALSE.  It will have then
   been set to pdTRUE only if reading from or writing to a queue caused a task
   of equal or greater priority than the currently executing task to leave the
@@ -324,3 +350,9 @@ ISR(TIMER4_COMPA_vect){
   from an ISR! */
   //portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
  }
+
+ ISR(TIMER4_COMPB_vect){
+ }
+
+
+
